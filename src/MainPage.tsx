e59cbfdd5ed69bd10bad95fef2477035d22938a6
cyw0626 <1916534795@qq.com>
2023-05-22 12:27:05 +0800

    feat: test

---
+++ src/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from 'antd';
+
+function MainPage() {
+    return (
+        <div>
+            <Button>Test</Button>
+        </div>
+    );
+}
+
+export default MainPage;

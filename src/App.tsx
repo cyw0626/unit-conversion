74c46bbaf9b498809a3595d0e9b8d1e5076b085a
cyw0626 <1916534795@qq.com>
2023-05-24 11:09:00 +0800

    fixL UI fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
-import { Layout, Menu, theme } from 'antd';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Layout, Menu, theme } from 'antd';
+import { CalculatorOutlined, LineChartOutlined, UserOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import Conversion from './components/Conversion/index.tsx';
 
 const { Header, Content, Sider } = Layout;
 
-const items: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-
-      children: new Array(2).fill(null).map((_, j) => {
-        const subKey = index * 2 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
-  },
-);
+const items: MenuProps['items'] = [{
+  key: 'Tools',
+  icon: <CalculatorOutlined />,
+  label: 'Tools',
+}, {
+  key: 'Demo',
+  icon: <LineChartOutlined />,
+  label: 'Demo',
+  children: new Array(2).fill(null).map((_, j) => ({
+      key: `Demo Option${j}`,
+      label: `Demo Option${j}`,
+  })),
+}];
 
 const App: React.FC = () => {
   const {
@@ -32,16 +26,19 @@ const App: React.FC = () => {
   } = theme.useToken();
 
   return (
-    <Layout>
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <img src={require('./asset/logo.jpg')} width="100px" id="logo" alt=""/>
+    <Layout style={{minHeight: '100vh'}}>
+      <Header>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 15 }}>
+          <img src={require('./asset/logo.jpg')} width="100px" id="logo" alt=""/>
+          <Avatar style={{ backgroundColor: '#f56a00', float: 'right' }}>S</Avatar>
+        </div>
       </Header>
-      <Layout style={{minHeight: '100vh'}}>
+      <Layout>
         <Sider width={200} style={{ background: colorBgContainer }}>
           <Menu
             mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
+            defaultSelectedKeys={['Tools']}
+            // defaultOpenKeys={['sub1']}
             style={{ height: '100%', borderRight: 0 }}
             items={items}
           />
--- src/components/Conversion/index.tsx
+++ src/components/Conversion/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import { Col, Row, Input, Select, Button, message, Tooltip } from 'antd';
-import { SwapOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { Col, Row, Input, Select, Button, message, Tooltip, Space, Statistic } from 'antd';
+import { SwapOutlined, QuestionCircleOutlined, RedoOutlined } from '@ant-design/icons';
 import { StreamData } from '../../steam-data-conversion/steam-data.ts';
 
 enum Unit {
@@ -41,17 +41,11 @@ const originOptions = [{
     value: Unit.F
 }];
 
-const evolutionOptions = [{
-    text: '千瓦时（KWh）',
-    value: Unit.KWh
-}];
 
 const findSuitVal = (originVal) => {
-    let temp = 0;
     let calData = StreamData[0] as any;
-    const data = StreamData.map((_, index) => {
+    StreamData.map((_, index) => {
         if (originVal == StreamData[index].temperature) {
-            temp = index;
             calData = {
                 "temperature": Number(_.temperature),
                 "Hf": Number(_.Hf),
@@ -60,8 +54,8 @@ const findSuitVal = (originVal) => {
             };
         }
 
-        if (originVal < StreamData[index].temperature && originVal > StreamData[index - 1].temperature) {
-            temp = index;
+        if ((StreamData[index]?.temperature && originVal < StreamData[index]?.temperature)
+            && (StreamData[index - 1]?.temperature && originVal > StreamData[index - 1]?.temperature)) {
             calData = {
                 "temperature": originVal,
                 "Hf": (Number(_.Hf) - Number(StreamData[index - 1].Hf)) / (Number(_.temperature) - Number(StreamData[index - 1].temperature)) * (originVal - Number(StreamData[index - 1].temperature)) + Number(StreamData[index - 1].Hf),
@@ -81,29 +75,25 @@ const findSuitVal = (originVal) => {
     }
     );
     return calData.Hg;
-    // console.log('dedede', temp, data[temp], calData);
 }
 
 const Conversion = () => {
     const [origin, setOrigin] = useState('');
-    const [btnDisabled, setBtnDisabled] = useState(true);
     const [originUnit, setOriginUnit] = useState(originOptions[0].value);
     const [weight, setWeight] = useState(1);
-    const [evolution, setEvolution] = useState('');
+    const [evolution, setEvolution] = useState(0);
 
     const originChange = (e) => {
         const val = e.target.value;
         setOrigin(val);
         if (val?.length && /^\d+(\.\d+)?$/.test(val)) {
-            setBtnDisabled(false);
         } else {
-            setBtnDisabled(true);
             return message.warning('请输入数字！');
         }
     };
 
     const onOriginUnitChange = (val) => {
-        setEvolution('');
+        setEvolution(0);
         setOriginUnit(val);
     }
 
@@ -139,14 +129,21 @@ const Conversion = () => {
                 evolutionVal = originVal * 1000 * 4.1868 / 3600000;
                 break;
             case Unit.C:
-                evolutionVal = findSuitVal(originVal) * weight / 3600;
+                evolutionVal = origin.length === 0 ? 0 : findSuitVal(originVal) * weight / 3600;
                 break;
             case Unit.F:
-                evolutionVal = findSuitVal(originVal - 273) * weight / 3600;
+                evolutionVal = origin.length === 0 ? 0 : findSuitVal(originVal - 273) * weight / 3600;
                 break;
         }
-        setEvolution(String(evolutionVal));
+        setEvolution(evolutionVal);
     };
+
+    const reset = () => {
+        setOrigin('');
+        setOriginUnit(originOptions[0].value);
+        setEvolution(0);
+        setWeight(1);
+    }
 
     return (
         <>
@@ -156,87 +153,71 @@ const Conversion = () => {
                 </Col>
             </Row>
             <Row>
-                <Col span={10}>
-                    <Input.TextArea
-                        size='large'
-                        placeholder='请输入要转换的值'
-                        rows={4}
-                        value={origin}
-                        onChange={originChange}
-                    />
-                </Col>
-                <Col span={4} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                    <Button
-                        type='primary'
-                        onClick={convert}
-                        disabled={btnDisabled}
-                        size='large'
-                        icon={<SwapOutlined />}
-                    >转换</Button>
-                </Col>
-                <Col span={10}>
-                    <Input.TextArea
-                        placeholder='转换结果'
-                        rows={4}
-                        disabled
-                        value={evolution}
-                        size='large'
-                    />
-                </Col>
-            </Row>
-            <Row style={{marginTop: 10}}>
-                <Col span={10} style={{ display: 'flex' }}>
-                    <Select
-                        style={{width: 220}}
-                        placeholder={'请选择要转换的单位'}
-                        size='large'
-                        showSearch
-                        optionFilterProp="children"
-                        value={originUnit}
-                        onChange={onOriginUnitChange}
-                        filterOption={(input, option) => ((option?.value ?? '') as String).toLowerCase().includes(input.toLowerCase())}
-                    >
-                        {
-                            originOptions.map(option => (
-                                <Select.Option value={option.value} key={option.value}>
-                                    {option.text}
-                                </Select.Option>
-                            ))
-                        }
-                    </Select>
-                    {
-                        (originUnit === Unit.C ||
-                        originUnit === Unit.F) && (
-                            <>
-                                <Input
-                                    style={{marginLeft: 20, width: 220}}
-                                    size='large'
-                                    addonAfter="Kg"
-                                    value={weight}
-                                    onChange={handleWeightChange}
-                                />
-                                <Tooltip title="默认为 1Kg 水蒸汽的能量">
-                                    <QuestionCircleOutlined style={{marginTop: 
-                                        10, marginLeft: 10}} />
-                                </Tooltip>
-                            </>
-                        )
-                    }
-                </Col>
-                <Col span={4}></Col>
-                <Col span={10}>
-                    <Select style={{width: 220}} defaultValue={evolutionOptions[0].value} size='large'>
-                        {
-                            evolutionOptions.map(option => (
-                                <Select.Option value={option.value} key={option.value}>
-                                    {option.text}
-                                </Select.Option>
-                            ))
-                        }
-                    </Select>
+                <Col span={24}>
+                    <Space.Compact size='large'>
+                        <Input
+                            placeholder='请输入待转换的值'
+                            value={origin}
+                            onChange={originChange}
+                        />
+                        <Select
+                            style={{width: 340}}
+                            placeholder={'请选择待转换的单位'}
+                            showSearch
+                            optionFilterProp="children"
+                            value={originUnit}
+                            onChange={onOriginUnitChange}
+                            filterOption={(input, option) => ((option?.value ?? '') as String).toLowerCase().includes(input.toLowerCase())}
+                        >
+                            {
+                                originOptions.map(option => (
+                                    <Select.Option value={option.value} key={option.value}>
+                                        {option.text}
+                                    </Select.Option>
+                                ))
+                            }
+                        </Select>
+                    </Space.Compact>
                 </Col>
             </Row>
-            <Row></Row>
+            {
+                (originUnit === Unit.C || originUnit === Unit.F) && (
+                    <Row style={{marginTop: 20}}>
+                        <Input
+                            style={{width: 220}}
+                            size='large'
+                            addonAfter="Kg"
+                            value={weight}
+                            onChange={handleWeightChange}
+                        />
+                        <Tooltip title="默认转换为 1Kg 水蒸汽的能量">
+                            <QuestionCircleOutlined
+                                style={{marginTop: 12, marginLeft: 10}}
+                            />
+                        </Tooltip>
+                    </Row>
+                )
+            }
+            <Row style={{marginTop: 20}}>
+                <Statistic
+                    title='电量（KWh）'
+                    value={evolution}
+                />
+            </Row>
+            <Row style={{marginTop: 20}}>
+                <Button
+                    type='primary'
+                    onClick={convert}
+                    size='middle'
+                    icon={<SwapOutlined />}
+                >转换</Button>
+                <Button
+                    onClick={reset}
+                    size='middle'
+                    icon={<RedoOutlined />}
+                    style={{ marginLeft: 20 }}
+                >重置</Button>
+            </Row>
         </>
     );
 }
